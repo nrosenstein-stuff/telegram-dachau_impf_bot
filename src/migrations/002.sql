a3daa13c51a9a79b59096277469afe3e03be2fac
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-06-19 12:09:39 +0200

    add drop tables to 002 migration, we populate that data anew anyway

--- src/migrations/002.sql
+++ src/migrations/002.sql
@@ -3,6 +3,9 @@
 ALTER TABLE user_registration ADD COLUMN subscription_active BOOLEAN NOT NULL DEFAULT TRUE;
 ALTER TABLE user_registration ADD COLUMN registered_at DATETIME;
 
+DROP TABLE vaccination_center_by_type;
+DROP TABLE available_day;
+
 CREATE TABLE IF NOT EXISTS schema_version (
   version INTEGER PRIMARY KEY
 );
