fe3b1aea612783d9b44ef3d1db9b5454a80e8078
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-06-20 21:29:04 +0200

    add docker-compose.yml, default db file in data/impfbot.db

--- src/impfbot/main/config.py
+++ src/impfbot/main/config.py
@@ -7,7 +7,7 @@ import databind.yaml as yaml
 class Config:
   token: str
   admin_chat_id: int = 56970700  # Niklas R.
-  database_spec: str = 'sqlite+pysqlite:///impfbot.db'
+  database_spec: str = 'sqlite+pysqlite:///data/impfbot.db'
   check_period: int = 20  # minutes
   log_format: str = '[%(asctime)s - %(levelname)s - %(name)s]: %(message)s'
 
