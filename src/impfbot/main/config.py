30bcb0f7b76f58baf41be7633e137488d62ddaf9
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-06-21 01:12:03 +0200

    update default check period (20 minutes now as before, but the config has to be in seconds now)

--- src/impfbot/main/config.py
+++ src/impfbot/main/config.py
@@ -8,7 +8,7 @@ class Config:
   token: str
   admin_chat_id: int = 56970700  # Niklas R.
   database_spec: str = 'sqlite+pysqlite:///data/impfbot.db'
-  check_period: int = 20  # minutes
+  check_period: int = 20 * 60  # 20 minutes
   log_format: str = '[%(asctime)s - %(levelname)s - %(name)s]: %(message)s'
 
   @classmethod
