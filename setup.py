4ab509c4b6d41e5df7796080ca32a8afc19304ea
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-06-22 19:16:20 +0200

    (impfbot) bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ test_requirements = [
 
 setuptools.setup(
   name = 'impfbot',
-  version = '1.0.0',
+  version = '1.1.0',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   description = 'Package description here.',
